07fab1013afab04b0a47befa1648307c9597ff38
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-07-04 22:28:20 +0200

    dependent destroys on all the models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
   belongs_to :user
   belongs_to :friend_user, class_name: 'User'
-  has_many :points
+  has_many :points, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, source: :friend_user
 
-  has_many :boards
-  has_many :points
+  has_many :boards, dependent: :destroy
+  has_many :points, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 
