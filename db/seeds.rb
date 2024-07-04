b03fd3baad94324a96f181d641fa0379b873c57f
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-07-04 23:02:59 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,23 @@ if Point.count == 0 && Board.count == 0 && Friendship.count == 0 && User.count =
 end
 
 user = User.create!(email: "meg@cat.com", password: "123456", username: "Mr Kitty")
-file = URI.open("https://res-console.cloudinary.com/dq1yih0di/thumbnails/v1/image/upload/v1720126053/bWlzdGVyX2tpdHR5X2FuZHJ3OA==/drilldown")
-user.photo.attach(io: file, filename: "mr_kitty.png", content_type: "image/png")
+file = URI.open("https://res.cloudinary.com/dq1yih0di/image/upload/fl_preserve_transparency/v1720126053/mister_kitty_andrw8.jpg?_s=public-apps")
+user.profile_picture.attach(io: file, filename: "mr_kitty.png", content_type: "image/png")
 user.save!
 user = User.create!(email: "ki@kitty.com", password: "secret", username: "Madame Cat")
-file = URI.open("https://res-console.cloudinary.com/dq1yih0di/thumbnails/v1/image/upload/v1720126381/bWFkYW1lX2NhdF9xYjVkYWc=/drilldown")
-user.photo.attach(io: file, filename: "madame_cat.png", content_type: "image/png")
+file = URI.open("https://res.cloudinary.com/dq1yih0di/image/upload/fl_preserve_transparency/v1720126381/madame_cat_qb5dag.jpg?_s=public-apps")
+user.profile_picture.attach(io: file, filename: "madame_cat.png", content_type: "image/png")
 user.save!
 
 puts "#{User.count} users created! #{User.first.username} and #{User.last.username}"
 
 Friendship.create!(user: User.first, friend_user: User.last)
 
-puts "#{Friendship.count} friendships created! #{Friendship.first.user.username} and #{Friendship.first.friend_user.username}"
+puts "#{Friendship.count} friendship created! #{Friendship.first.user.username} and #{Friendship.first.friend_user.username}"
 
 Board.create!(name: "Badgammon", user: User.first, friend_user_id: User.last.id, game: "Backgammon")
 
-puts "#{Board.count} boards created! #{Board.first.name} by #{Board.first.user.username} with #{Board.first.friend_user.username}"
+puts "#{Board.count} board created! #{Board.first.name} by #{Board.first.user.username} with #{Board.first.friend_user.username}"
 
 Point.create!(board: Board.first, user: User.first, value: 3)
 Point.create!(board: Board.first, user: User.last, value: 1)
