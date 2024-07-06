bac6e92ebedc4b46c024211fe2080817e48d445e
Megan Street <meganjstreet@gmail.com>
2024-07-06 15:58:05 +0200

    reject users from @everyone that have a friendship with current user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,11 @@ class PagesController < ApplicationController
   # at some point exclude users that are already added as friends
   def everyone
     @everyone = User.where.not(id: current_user.id)
+    @everyone = @everyone.reject do |user|
+      user.all_friendships.any? do |friendship|
+        [friendship.friend_user_id, friendship.user_id].include?(current_user.id)
+      end
+    end
   end
 
 end
